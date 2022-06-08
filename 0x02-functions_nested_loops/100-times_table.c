4c587bfd72e4f7612b4a23810afeac863ac74297
root <mykeloriyomi@gmail.com>
2022-06-08 00:12:16 -0700

    a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,31 +20,31 @@ void print_times_table(int n)
 			if (z > 99)
 			{
 				_putchar(z / 100 + '0');
-	        		_putchar((z / 10 % 10) + '0');
-	        		_putchar(z % 10 + '0');
-	        	}
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+			}
 			else if (z > 9)
 			{
 				_putchar(' ');
-	      			_putchar(z / 10 + '0');
-	      			_putchar(z % 10 + '0');
-	        	}
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
 			else if (y != 0)
 			{
-	       			_putchar(' ');
-	      			_putchar(' ');
-	      			_putchar(z + '0');
-	      		}
-	  		else
-	    			_putchar(z + '0');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
 
-	  		if (y != n)
-	    		{
-	      			_putchar(',');
-	      			_putchar(' ');
-	    		}
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-      		_putchar('\n');
-      		x++;
+		_putchar('\n');
+		x++;
 	}
 }
