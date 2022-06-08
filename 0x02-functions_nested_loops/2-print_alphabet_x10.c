3eff438f42d48298390f938f1a9f65df4d61d77f
root <mykeloriyomi@gmail.com>
2022-06-07 19:59:00 -0700

    Task 13

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code.
- * Return: Always 0.
+ * print - print_alphabet_x10
+ *Description : Printing alphabets x_10
+ * Return: 0
  */
 
 void print_alphabet_x10(void)
