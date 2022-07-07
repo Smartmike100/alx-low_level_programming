2535d6c26ea7e5f5fc8b9b57a3c8079e9b9ae828
root <mykeloriyomi@gmail.com>
2022-07-06 17:29:55 -0700

    Fixing Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
-#include <stdlib.h>
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_name - prints a name
- * @name: name to print
- * @f: pointer to the printing function
+ * @name: name of a person
+ * @f:pointer to print function
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
-	f(name);
+if (name && f)
+f(name);
 }
